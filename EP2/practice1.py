c1cdbb613533e6e8861b71302dfc42405a011b9c
Inter Somsith <62319067+somsith201902@users.noreply.github.com>
2020-09-22 15:15:16 +0700

    Winter

--- EP2/practice1.py
+++ EP2/practice1.py
@@ -18,21 +18,24 @@ B1 = Entry(GUI, textvariable=v_product)
 B1.pack()
 
 ### ROW 2 ###
-L2 = Label(GUI, text="Product", font=font1)
+L2 = Label(GUI, text="Price", font=font1)
 L2.pack()
 
-v_product = StringVar()
+v_price = StringVar()
 
-B2 = Entry(GUI, textvariable=v_product)
+B2 = Entry(GUI, textvariable=v_price)
 B2.pack()
 
-### ROW 1 ###
-L = Label(GUI, text="Product", font=font1)
-L1.pack()
+### ROW 3 ###
+L3 = Label(GUI, text="Amount", font=font1)
+L3.pack()
 
-v_product = StringVar()
+v_amount = StringVar()
 
-B1 = Entry(GUI, textvariable=v_product)
+B1 = Entry(GUI, textvariable=v_amount)
 B1.pack()
 
+### Button ###
+but1 = Button(GUI, text="Calculater")
+but1.pack(pady=10)
 GUI.mainloop()
